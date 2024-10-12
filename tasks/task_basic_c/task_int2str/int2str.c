27e00a405794566e37b55ab32190522d961f27ca
Sergey <rmpo2247@gmail.com>
2024-10-12 10:23:33 +0300

    HW1

--- tasks/task_basic_c/task_int2str/int2str.c
+++ tasks/task_basic_c/task_int2str/int2str.c
@@ -1,8 +1,21 @@
+#include <stdio.h>
 #include <assert.h>
 #include "int2str.h"
-#include "stdio.h"
+#define MAX_LENGTH 100
 
-char* int2str(int number) {
-    char* str = "0";
-    return str;
+int main() {
+    char str[MAX_LENGTH]; 
+    int num = 0;
+    int i = 0; 
+
+    printf("Введите строку: ");
+    scanf("%s", str); 
+
+    while (str[i] >= '0' && str[i] <= '9') { 
+        num = num * 10 + (str[i++] - '0'); 
+    }
+    printf("%d\n", num);
+
+    return 0;
 }
+
--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -1,7 +1,29 @@
+#include <stdio.h>
 #include <assert.h>
 #include "str2int.h"
-#include "stdio.h"
+#define MAX_LENGTH 100
 
-int str2int(const char *str) {
-    return 0;
+int str2int(const char* str) {
+    int num = 0;
+    int i = 0; 
+    while (str[i] >= '0' && str[i] <= '9') { 
+        num = num * 10 + (str[i++] - '0'); 
+    }
+
 }
+
+// int main() {
+//     // char str[MAX_LENGTH]; 
+//     // int num = 0;
+//     // int i = 0; 
+
+//     //printf("Введите строку: ");
+//     //scanf("%s", str); 
+//     // char* str = "123";
+//     // while (str[i] >= '0' && str[i] <= '9') { 
+//     //     num = num * 10 + (str[i++] - '0'); 
+//     // }
+//     printf("%d\n", str2int("123"));
+
+//     return 0;
+// }
